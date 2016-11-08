87df75df5e216bb624c47f601072d2020965cacb
Abhishek Chowdhury <Portal9001@abhisheks-mbp.attlocal.net>
2016-11-07 21:25:15 -0800

    Reducers

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,7 @@
+function comments(state=[], action){ // state= [] is es6 to set into default
+	console.log(state, action)
+	return state;
+}
+
+
+export default comments;
---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,7 @@
+function posts(state=[], action){ // state= [] is es6 to set into default
+	console.log(state, action)
+	return state;
+}
+
+
+export default posts;

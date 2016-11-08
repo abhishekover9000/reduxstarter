639ffde7030d6bd80e3712a180842b5569ecb03d
Abhishek Chowdhury <Portal9001@abhisheks-mbp.attlocal.net>
2016-11-07 20:04:19 -0800

    First two lectures

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,14 @@
 // let's go!
+// import react
+import React from 'react';
+import { render } from 'react-dom';
+
+// import css
+
+import css from './styles/style.styl';
+
+// import component
+
+import Main from './components/Main';
+
+render(<Main/>, document.getElementById('root'));
